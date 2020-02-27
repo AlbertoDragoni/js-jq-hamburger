3b67833d05408ad0c5a77eb0cd93013764081b1c
Alberto Dragoni <albertodragoni@yahoo.it>
2020-02-27 16:59:18 +0100

    No fatto in un modo solo

--- js/main.js
+++ js/main.js
@@ -1,21 +1,7 @@
-//Modo 1 -------------------------
 
-// $('.fa-bars').on({
-//     click: function (){
-//         $('.fa-bars').hide();
-//         $('.hamburger-menu').show();
-//     }
-// })
-
-//Modo 2 -----------------------
-// $('.fa-bars').on({
-//     click: function (){
-//         $('.fa-bars').addClass('invisible');
-//         $('.hamburger-menu').addClass('visibile');
-//     }
-// })
-
-//Modo 3 ----------------------
-// $( ".fa-bars" ).click(function() { // seleziono l'icona tra ""
-//   $( ".hamburger-menu" ).addClass( "active" )
-// });
+$( ".fa-bars" ).click(function() { // seleziono l'icona tra ""
+  $( ".hamburger-menu" ).addClass( "active" );
+});
+$( ".hamburger-menu" ).click(function() { // seleziono l'icona tra ""
+  $( ".hamburger-menu" ).removeClass( "active" );
+});
