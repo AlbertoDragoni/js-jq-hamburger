012c6618aeadc26a8b94c857c081484cb74b2a19
Alberto Dragoni <albertodragoni@yahoo.it>
2020-02-27 16:45:44 +0100

    Fatto in 3 modi diversi ;-)

---
+++ js/main.js
@@ -0,0 +1,21 @@
+//Modo 1 -------------------------
+
+// $('.fa-bars').on({
+//     click: function (){
+//         $('.fa-bars').hide();
+//         $('.hamburger-menu').show();
+//     }
+// })
+
+//Modo 2 -----------------------
+// $('.fa-bars').on({
+//     click: function (){
+//         $('.fa-bars').addClass('invisible');
+//         $('.hamburger-menu').addClass('visibile');
+//     }
+// })
+
+//Modo 3 ----------------------
+// $( ".fa-bars" ).click(function() { // seleziono l'icona tra ""
+//   $( ".hamburger-menu" ).addClass( "active" )
+// });
